0afff05d5e71db3ac5cddf84316c0b422ace8a4a
Aybuke-Filiz <aybukeyavuz123@gmail.com>
2024-10-09 17:42:38 +0300

    header devam ediyor

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor:{
+        "brandColor":"#5d3ebc"
+      }
+    },
   },
   plugins: [],
 }
