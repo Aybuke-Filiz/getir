35e26b2490d2d67ba57cbda4a4839d94e7b723de
Aybuke-Filiz <aybukeyavuz123@gmail.com>
2024-10-10 16:58:52 +0300

    HeroSection geliştirmeleri devam ediyor

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,28 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 
 function HeroSection() {
+   const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
-    <div>HeroSection</div>
-  )
+    <div className="relative   before:bg-gradient-to-r before:from-primary-brand-color before:to-transparent before:absolute before:inset-0 before:w-full before:h-full">
+    <Slider {...settings}>
+      <div>
+     <img  className="w-full h-[500px] object-cover" src="https://swipeline.co/cdn/2023/03/kvkkgetirbitaksisahibinden.webp"/>
+     </div>
+     <div>
+      <img className="w-full h-[500px]  object-cover" src="https://www.getirfinans.com/_next/image/?url=%2F_next%2Fstatic%2Fmedia%2Fbanner_slide_001.ca9d4d4a.jpg&w=3840&q=75"/>
+     </div>
+    </Slider>
+    </div>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
   theme: {
     extend: {
       backgroundColor:{
-        "brandColor":"#5d3ebc"
+        "brand-color":"#4c3398",
+        "primary-brand-color":"#5d3ebc",
+        "secondary-brand-color":"#7849f7",
+
+      },
+      gradientColorStops:{
+        "brand-color":"#4c3398",
+        "primary-brand-color":"#5d3ebc",
+        "secondary-brand-color":"#7849f7",
+
       }
     },
   },
