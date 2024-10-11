f02a32d6f75eb3baecf2138401e9f2d59d9620ea
Aybuke-Filiz <aybukeyavuz123@gmail.com>
2024-10-12 01:22:33 +0300

    hero section tamamlandı

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,7 +6,16 @@ import { useState } from "react";
 
 
 function HeroSection() {
-  const [selected, setSelected] = useState("");
+  const [selected, setSelected] = useState("TR");
+
+  const phones={
+    US:"+1",
+    GB:"+44",
+    FR:"+33",
+    DE:"+49",
+    IT:"+70",
+    TR:"+90",
+  };
 
    const settings = {
     dots: false,
@@ -33,12 +42,27 @@ function HeroSection() {
                     </h3>
     </div>
     <div className="w-[400px] rounded-lg bg-gray-50 p-6">
-      <h4 className="text-primary-brand-color text-center font-semibold">Giriş yap veya kayıt ol</h4>
-      <div className="flex">
+      <h4 className="text-primary-brand-color text-center font-semibold mb-4">Giriş yap veya kayıt ol</h4>
+      <div className="grid gap-y-4">
+      <div className="flex gap-x-2">
         <ReactFlagsSelect
-      countries={["US", "GB", "FR", "DE", "IT"]}
-       customLabels={{ US: "EN-US", GB: "EN-GB", FR: "FR", DE: "DE", IT: "IT" }}
-        placeholder="Select Language"/>
+      countries={Object.keys(phones)}
+       customLabels={phones}
+        onSelect={(code) => setSelected(code)}
+        selected={selected}
+        className="flag-select"/>
+        <label className="flex-1 relative block cursor-pointer">
+          <input required className="h-14 px-4 border-2 border-gray-200 rounded w-full transition-colors hover:border-primary-brand-color 
+          outline-none focus:border-primary-brand-color peer text-sm pt-2"/>
+          <span className="absolute top-0 left-0 h-full px-4 flex items-center text-sm text-gray-500 transition-all peer-focus:h-7
+           peer-focus:text-primary-brand-color peer-focus:text-sm  peer-valid:h-7 peer-valid:text-primary-brand-color
+            peer-valid:text-sm">Telefon Numarası</span>
+        </label>
+</div>
+<button className="bg-brand-yellow h-12 flex items-center justify-center rounded-md w-full text-sm font-semibold text-primary-brand-color 
+transition-colors hover:bg-primary-brand-color hover:text-brand-yellow">
+  Telefon numarası ile devam et
+</button>
 </div>
     </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {
-      backgroundColor:{
+      colors:{
         "brand-color":"#4c3398",
         "primary-brand-color":"#5d3ebc",
         "secondary-brand-color":"#7849f7",
-
-      },
-      gradientColorStops:{
-        "brand-color":"#4c3398",
-        "primary-brand-color":"#5d3ebc",
-        "secondary-brand-color":"#7849f7",
-
-      },
-      textColor:{
-        "brand-color":"#4c3398",
-        "primary-brand-color":"#5d3ebc",
-        "secondary-brand-color":"#7849f7",
-
+        "brand-yellow":"#ffd300",
       }
+      
     },
   },
   plugins: [],
