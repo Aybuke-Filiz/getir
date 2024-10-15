b44603cb726f18a83bba28a9eab563d34b4e931f
Aybuke-Filiz <aybukeyavuz123@gmail.com>
2024-10-15 17:00:17 +0300

    favoriler tamamlandı

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,9 +1,26 @@
 
+import Products from "../api/products.json"
+import { useState } from "react";
+import { useEffect } from "react";
+import ProductItem from "./ui/ProductItem";
+import Title from "./ui/Title";
 
 function Favorites() {
-  return (
-    <div>Favorites</div>
-  )
+
+const [products,setProducts]=useState([]);
+
+useEffect(() => {
+  setProducts(Products);
+}, [])
+
+return (
+  <div className="container mx-auto">
+      <Title>Favoriler</Title>
+      <div className="grid grid-cols-8 gap-0.1 rounded-lg overflow-hidden">
+          {products.length && products.map(product => <ProductItem key={product.id} product={product} />)}
+      </div>
+  </div>
+)
 }
 
 export default Favorites
