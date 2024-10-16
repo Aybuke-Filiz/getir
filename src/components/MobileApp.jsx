1bc8a85116cc5bca5b060b16d34ebb378c04a544
Aybuke-Filiz <aybukeyavuz123@gmail.com>
2024-10-16 22:21:15 +0300

    mobile app tamamlandı

--- src/components/MobileApp.jsx
+++ src/components/MobileApp.jsx
@@ -8,13 +8,13 @@ function MobileApp() {
        <h3 className="text-2xl font-bold tracking-tight">Getir'i indirin!</h3>
        <p className="font-semibold">İstediğiniz ürünleri dakikalar içinde kapınıza</p>
        <nav className="mt-5 flex gap-x-2">
-        <a href="#">
+        <a href="#" className="transition-all transform hover:scale-105">
           <img src="https://getir.com/_next/static/images/appstore-tr-141ed939fceebdcee96af608fa293b31.svg"/>
         </a>
-        <a href="#">
+        <a href="#" className="transition-all transform hover:scale-105">
           <img src="https://getir.com/_next/static/images/googleplay-tr-6b0c941b7d1a65d781fb4b644498be75.svg"/>
         </a>
-        <a href="#">
+        <a href="#" className="transition-all transform hover:scale-105">
           <img src="https://getir.com/_next/static/images/huawei-appgallery-tr-4b890fa3167bc62f9069edaf45aa7f30.svg"/>
         </a>
        </nav>
